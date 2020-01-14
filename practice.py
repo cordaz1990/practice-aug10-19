82272c03423a86d7fd1505811f6f5bdb0f45bcb2
Cynthia Ordaz <cordaz@dons.usfca.edu>
2020-01-13 22:36:02 -0800

    Update practice.py

--- practice.py
+++ practice.py
@@ -1,3 +1,23 @@
-print('hello')
-print('work')
-print('world')
+#Looping Dictionaries 
+
+def print_hist(h):
+    for c in h:
+        print(c, h[c])
+        
+       
+>>> h = histogram('parrot')
+>>> print_hist(h)
+a 1
+p 1
+r 2
+t 1
+o 1
+
+for key in sorted(h):
+    print(key, h[key])
+    
+a 1
+o 1
+p 1
+r 2
+t 1 
